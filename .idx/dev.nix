b085650291ac6444ba955f23e19240d5e14a8c0c
0gravityshow <0gravityshow@gmail.com>
2026-02-07 00:58:51 -0500

    Configure Android environment

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,14 +3,14 @@
   
   packages = [
     pkgs.jdk17
-    pkgs.gradle
     pkgs.nodejs_20
   ];
 
+  env = {};
+  
   idx = {
     extensions = [
-      "Dart-Code.flutter"
-      "msjsdiag.debugger-for-chrome"
+      "fwcd.kotlin"
     ];
     
     workspace = {
@@ -21,12 +21,7 @@
     
     previews = {
       enable = true;
-      previews = {
-        android = {
-          command = ["./gradlew", "assembleDebug"];
-          manager = "android";
-        };
-      };
+      previews = {};
     };
   };
   
@@ -35,16 +30,5 @@
     flutter.enable = false;
     googleApis.enable = true;
     googlePlayServices.enable = true;
-    
-    # Android SDK settings
-    platforms = ["android-35"];
-    buildTools = ["35.0.0"];
-    systemImages = ["system-images;android-35;google_apis;x86_64"];
-    
-    # Enable emulator
-    emulator = {
-      enable = true;
-      device = "pixel_8";
-    };
   };
 }
