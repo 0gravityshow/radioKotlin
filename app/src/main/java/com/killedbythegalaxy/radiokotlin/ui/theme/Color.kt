9efbf172ab7f623317126dd1a92400d8727b97cf
0gravityshow <0gravityshow@gmail.com>
2026-02-07 00:38:01 -0500

    Add app/src/main/java/com/killedbythegalaxy/radiokotlin/ui/theme/Color.kt

---
+++ app/src/main/java/com/killedbythegalaxy/radiokotlin/ui/theme/Color.kt
@@ -0,0 +1,50 @@
+package com.killedbythegalaxy.radiokotlin.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Space Colors (from web player)
+val CyberCyan = Color(0xFF00FFFF)
+val CyberCyanDark = Color(0xFF00CCCC)
+val CyberBlue = Color(0xFF002244)
+val DeepSpace = Color(0xFF001122)
+val VoidBlack = Color(0xFF000000)
+
+// Status Colors
+val StatusLive = Color(0xFF00FF00)
+val StatusConnecting = Color(0xFFFFFF00)
+val StatusReconnecting = Color(0xFFFF8800)
+val StatusOffline = Color(0xFFFF0000)
+
+// Accent Colors
+val NebulaPurple = Color(0xFF6600FF)
+val NebulaPink = Color(0xFFFF00FF)
+val StarWhite = Color(0xFFFFFFFF)
+val CosmicGray = Color(0xFF1A1A2E)
+
+// Gradient Colors
+val GradientStart = Color(0xFF000428)
+val GradientMiddle = Color(0xFF004E92)
+val GradientEnd = Color(0xFF000428)
+
+// Chat Colors
+val ChatBubbleUser = Color(0xFF1A1A3E)
+val ChatBubbleOther = Color(0xFF0A0A1E)
+val EgoBotColor = Color(0xFF00FFFF)
+
+// Paywall Colors
+val PaywallGold = Color(0xFFFFD700)
+val PaywallPurple = Color(0xFF9C27B0)
+
+// Black Hole (Ban) Colors
+val EventHorizon = Color(0xFF000000)
+val SingularityPurple = Color(0xFF1A0033)
+
+// Material3 Dark Theme Colors
+val PrimaryDark = CyberCyan
+val OnPrimaryDark = VoidBlack
+val SecondaryDark = NebulaPurple
+val OnSecondaryDark = StarWhite
+val BackgroundDark = VoidBlack
+val SurfaceDark = CosmicGray
+val OnSurfaceDark = StarWhite
+val ErrorDark = StatusOffline
